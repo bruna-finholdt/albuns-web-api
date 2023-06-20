1121a08ab35113576fa3b069de63bbaefc8c6f03
Bruna Finholdt Lamboglia Perez <bruna.lamboglia@nutrien.com>
2023-06-20 09:37:17 -0300

    criando albuns controller

---
+++ PrimeiraWebAPI/Controllers/AlbunsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PrimeiraWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlbunsController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<String> Get()
+        {
+            List<string> listaDeDiscos = new List<string>();
+            listaDeDiscos.Add("s");
+            return listaDeDiscos;
+        }
+
+        [HttpGet("{id}")]
+        public String GetById(int id)
+        {
+            return "Album tal " + id;
+        }
+
+        [HttpGet("nome/{nome}")]
+        public String GetByNome(string nome) // nome do parametro deve ser o mesmo do {}
+        {
+            return "Album tal " + nome;
+        }
+    }
+}
