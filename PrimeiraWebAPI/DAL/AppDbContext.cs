11d852fd5b09d79d63e33f1010b213d36c2e65ae
Bruna Finholdt Lamboglia Perez <bruna.lamboglia@nutrien.com>
2023-06-23 13:00:14 -0300

    coments in appDbContext

--- PrimeiraWebAPI/DAL/AppDbContext.cs
+++ PrimeiraWebAPI/DAL/AppDbContext.cs
@@ -20,19 +20,20 @@ namespace PrimeiraWebAPI.DAL
         {
             modelBuilder.Entity<Album>(entity =>
             {
-                entity.Property(x => x.Nome).IsUnicode(false);
-                entity.Property(x => x.Artista).IsUnicode(false);
+                entity.Property(x => x.Nome).IsUnicode(false);//só para atributos do tupo string
+                entity.Property(x => x.Artista).IsUnicode(false);//só para atributos do tupo string
             });
 
             modelBuilder.Entity<Avaliacao>(entity =>
             {
-                entity.Property(x => x.Comentario).IsUnicode(false);
+                entity.Property(x => x.Comentario).IsUnicode(false);//só para atributos do tupo string
                 entity.HasOne(x => x.Album) // entidade virtual que indica relação
                 .WithMany(y => y.Avaliacoes) // coleção na entidade Album
                 .HasForeignKey(x => x.IdAlbum) //IdAlbum da entidade avaliacao
                 .OnDelete(DeleteBehavior.Cascade); // cascade delete - quando um album for removido, suas avaliações relacionadas também serão
             });
-        }
+        }            //hasone album - withmany avaliacoes - FK Idalbum - se deletar um album, deleta em
+                     //cascata todas as avaliações (é isso?)
     }
 }
 
